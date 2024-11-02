dea8f7e02df9afd11b004ce61b4cb604af44888c
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-02 16:19:01 +0700

    mã hóa mật khẩu và matching với Bcrypt

--- springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/dto/request/APIResponse.java
+++ springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/dto/request/APIResponse.java
@@ -2,13 +2,14 @@ package com.gahoccode.identity_service.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.mapstruct.Builder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class APIResponse <T>{
     private int code = 1000;
