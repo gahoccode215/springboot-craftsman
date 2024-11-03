5d11cbfbadee585f6c3ef903452eab98c5d571c0
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-04 03:05:10 +0700

    quản lý Exception và trả về Response với HttpCode tùy biến

--- springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/configuration/SecurityConfig.java
+++ springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/configuration/SecurityConfig.java
@@ -41,9 +41,10 @@ public class SecurityConfig {
 
         httpSecurity.oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwtConfigurer ->
-                        jwtConfigurer.decoder(jwtDecoder())
-                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                );
+                                jwtConfigurer.decoder(jwtDecoder())
+                                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+        );
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
--- springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/exception/GlobalExceptionHandler.java
+++ springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/exception/GlobalExceptionHandler.java
@@ -7,38 +7,49 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = KeyLengthException.class)
-    ResponseEntity<APIResponse> handlingKeyLengthException(KeyLengthException exception){
+    ResponseEntity<APIResponse> handlingKeyLengthException(KeyLengthException exception) {
         APIResponse apiResponse = new APIResponse();
         apiResponse.setCode(ErrorCode.KEY_LENGTH.getCode());
         apiResponse.setMessage(ErrorCode.KEY_LENGTH.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
     }
+
     @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<APIResponse> handlingRuntimeException(RuntimeException exception){
+    ResponseEntity<APIResponse> handlingRuntimeException(RuntimeException exception) {
         APIResponse apiResponse = new APIResponse();
         apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
         apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
     }
+
     @ExceptionHandler(value = AppException.class)
-    ResponseEntity<APIResponse> handlingValidation(AppException exception){
+    ResponseEntity<APIResponse> handlingAppException(AppException exception) {
         ErrorCode errorCode = exception.getErrorCode();
         APIResponse apiResponse = new APIResponse();
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
     }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<APIResponse> handlingAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        return ResponseEntity.status(errorCode.getStatusCode()).body(APIResponse.builder().code(errorCode.getCode()).message(errorCode.getMessage()).build());
+    }
+
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<APIResponse> handlingValidation(MethodArgumentNotValidException exception){
+    ResponseEntity<APIResponse> handlingValidation(MethodArgumentNotValidException exception) {
         String enumKey = exception.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
 
         }
         APIResponse apiResponse = new APIResponse();
