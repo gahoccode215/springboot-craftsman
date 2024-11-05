38248460230d1aaa3a1e2efc610bf42c66216bae
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-05 10:24:48 +0700

    Validation nâng cao, trích xuất thông tin từ annotation

--- springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/exception/ErrorCode.java
+++ springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/exception/ErrorCode.java
@@ -14,12 +14,12 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1001, "Uncategorized error.", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1002, "User existed.", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters.", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004, "Password must be at least 8 characters.", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least {min} characters.", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Password must be at least {min} characters.", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005, "User existed.", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006, "Unauthenticated.", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "You do not have permission.", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Invalid date of birth", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1008, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
     KEY_LENGTH(1009, "The secret length must be at least 256 bits.", HttpStatus.BAD_REQUEST)
     ;
     private int code;
--- springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/exception/GlobalExceptionHandler.java
+++ springboot-3/2-identity-service/src/main/java/com/gahoccode/identity_service/exception/GlobalExceptionHandler.java
@@ -2,15 +2,20 @@ package com.gahoccode.identity_service.exception;
 
 import com.gahoccode.identity_service.dto.request.APIResponse;
 import com.nimbusds.jose.KeyLengthException;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.nio.file.AccessDeniedException;
+import java.util.Map;
+import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
 
     @ExceptionHandler(value = KeyLengthException.class)
     ResponseEntity<APIResponse> handlingKeyLengthException(KeyLengthException exception) {
@@ -47,14 +52,22 @@ public class GlobalExceptionHandler {
     ResponseEntity<APIResponse> handlingValidation(MethodArgumentNotValidException exception) {
         String enumKey = exception.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+            var constrainViolation = exception.getBindingResult()
+                    .getAllErrors().get(0).unwrap(ConstraintViolation.class);
+            attributes = constrainViolation.getConstraintDescriptor().getAttributes();
         } catch (IllegalArgumentException e) {
 
         }
         APIResponse apiResponse = new APIResponse();
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ? mapAttribute(errorCode.getMessage(), attributes) : errorCode.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+    private String mapAttribute(String message, Map<String, Object> attributes){
+        String minValue = (String) attributes.get(MIN_ATTRIBUTE);
+        return message.replace("{"+ MIN_ATTRIBUTE + "}", minValue);
     }
 }
