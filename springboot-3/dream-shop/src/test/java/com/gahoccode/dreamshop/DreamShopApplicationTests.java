97a23c63a79b61a73cea0dc86b049ca987778e50
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-12 11:24:45 +0700

    init project Shopping Cart for practice Spring Boot, Spring Security, JWT

---
+++ springboot-3/dream-shop/src/test/java/com/gahoccode/dreamshop/DreamShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gahoccode.dreamshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
