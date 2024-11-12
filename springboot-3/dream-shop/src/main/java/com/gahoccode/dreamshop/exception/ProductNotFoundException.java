d40e2d079ac5f26946c2e86b4a1f263d1d707317
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-12 12:15:27 +0700

    springboot-3 dream-shop creating and mapping entity classes

---
+++ springboot-3/dream-shop/src/main/java/com/gahoccode/dreamshop/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gahoccode.dreamshop.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ springboot-3/dream-shop/src/main/java/com/gahoccode/dreamshop/service/product/ProductService.java
@@ -0,0 +1,71 @@
+package com.gahoccode.dreamshop.service.product;
+
+import com.gahoccode.dreamshop.exception.ProductNotFoundException;
+import com.gahoccode.dreamshop.model.Product;
+import com.gahoccode.dreamshop.repository.product.ProductRepository;
+
+import java.util.List;
+
+public class ProductService implements IProductService{
+
+
+    private ProductRepository productRepository;
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found!"));
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.findById(id).ifPresentOrElse(productRepository::delete, () -> {
+            throw new ProductNotFoundException("Product not found!");
+        });
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategoryName(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return productRepository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return productRepository.findByCategoryNameAndBrand(category, brand);
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return productRepository.findByBrandAndName(brand, name);
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return productRepository.countByBrandAndName(brand, name);
+    }
+}
