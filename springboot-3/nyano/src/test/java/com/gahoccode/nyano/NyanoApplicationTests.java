d0de369b32d8304e2a7d262ee869e79def49fd4d
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-16 22:12:59 +0700

    springboot-3 nyano create user entity

---
+++ springboot-3/nyano/src/test/java/com/gahoccode/nyano/NyanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gahoccode.nyano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
