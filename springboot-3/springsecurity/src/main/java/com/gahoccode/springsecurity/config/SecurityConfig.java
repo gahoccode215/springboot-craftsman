214eeec8a36248ced36323c90feeabd4fb755d9f
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-20 23:55:07 +0700

    springboot-ddd-architecture khởi tạo dự án theo kiến trúc DDD

--- springboot-3/springsecurity/src/main/java/com/gahoccode/springsecurity/config/SecurityConfig.java
+++ springboot-3/springsecurity/src/main/java/com/gahoccode/springsecurity/config/SecurityConfig.java
@@ -2,16 +2,29 @@ package com.gahoccode.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin((formlogin) -> formlogin.loginProcessingUrl("/login"));
+        http.authorizeHttpRequests(req -> req
+                .requestMatchers("/auth/login", "/auth/register")
+                .permitAll()
+                .anyRequest().authenticated());
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
